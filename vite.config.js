aa11481eb9a647edf25f85f8e36b93ba60ff65ea
Nazar <nazar.shevchuk.121@gmail.com>
2026-06-23 20:55:28 +0200

    added settings for gh-pages2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/buttle/",
+  base: "/buttle/Home/",
   plugins: [react(), tailwindcss()],
 });
